bd85fff08c36e840c614b022bb1063daf4980063
djb56 <66229768+djb56@users.noreply.github.com>
2020-06-03 16:38:15 +1200

    Full UPDATE

--- src/components/Zip.js
+++ src/components/Zip.js
@@ -5,15 +5,8 @@ function Zip(props) {
     const [validationError, setValidationError] = useState(null);
 
     const validate = (event) => {
-        const zipCodePattern = /^\d{5}$/;
-        const valid = zipCodePattern.test(event.target.value);
-        if (!valid) {
-            setValidationError('* should be a 5 digit number only');
-            props.clearResponse();
-        } else {
             setValidationError('');
             props.onZipChange(event.target.value);
-        }
     };
 
     return (
@@ -30,7 +23,7 @@ function Zip(props) {
                         type="text" 
                         className="form-control" 
                         id="usr" 
-                        placeholder="US Zip Code (5 digit)"
+                        placeholder="NZ CITY"
                         onKeyPress={(event) => {
                             if (event.key === "Enter") {
                                 validate(event);
